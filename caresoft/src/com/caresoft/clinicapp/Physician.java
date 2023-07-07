eeb19dc1d337b19d9d8ba3c42632de7b24831e5e
Michael Smith <mjusmith1@gmail.com>
2023-07-06 22:51:06 -0500

    ini.com.

---
+++ caresoft/src/com/caresoft/clinicapp/Physician.java
@@ -0,0 +1,49 @@
+package com.caresoft.clinicapp;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Physician extends User implements HIPAACompliantUser {
+	private ArrayList<String> patientNotes;
+	
+	// TO DO: Constructor that takes an IDcopy
+	public Physician(int id) {
+		this.id=id;
+	}
+	
+    // TO DO: Implement HIPAACompliantUser!
+	@Override
+	public boolean assignPin(int pin) {
+		if(pin >= 1000 && pin <= 9999) {
+		this.pin=pin;
+		return true;
+		}
+		else return false;
+	}
+	@Override
+	public boolean accessAuthorized(Integer confirmedAuthID) {
+		if(this.id == confirmedAuthID) {
+			return true;
+		}
+		else return false;
+	}
+	
+	public void newPatientNotes(String notes, String patientName, Date date) {
+        String report = String.format(
+            "Datetime Submitted: %s \n", date);
+        report += String.format("Reported By ID: %s\n", this.id);
+        report += String.format("Patient Name: %s\n", patientName);
+        report += String.format("Notes: %s \n", notes);
+        this.patientNotes.add(report);
+    }
+
+	// TO DO: Setters & Getters
+	public ArrayList<String> getPatientNotes() {
+		return patientNotes;
+	}
+
+	public void setPatientNotes(ArrayList<String> patientNotes) {
+		this.patientNotes = patientNotes;
+	}
+
+}
