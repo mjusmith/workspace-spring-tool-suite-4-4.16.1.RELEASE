aa5a6ae59285b9e27dfc162a38bbde29e64bbe33
Michael Smith <mjusmith1@gmail.com>
2023-07-09 15:22:56 -0500

    ini.com.

--- daikichi/src/main/java/com/example/demo/controllers/TestController.java
+++ daikichi/src/main/java/com/example/demo/controllers/TestController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,5 +20,18 @@ public class TestController {
 	@GetMapping("/daikichi/tomorrow") // @PostMapping , @PutMapping , @DeleteMapping
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	@GetMapping("/daikichi/travel/{destination}") // @PostMapping , @PutMapping , @DeleteMapping
+	public String travel(@PathVariable("destination") String destination) {
+		return "Congratulations! You will soon travel to " + destination + "!";
+	}
+	
+	@GetMapping("/daikichi/lotto/{number}") // @PostMapping , @PutMapping , @DeleteMapping
+	public String lotto(@PathVariable("number") Integer number) {
+		if(number % 2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers.";
+		}
+		else return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
 	}
 }
